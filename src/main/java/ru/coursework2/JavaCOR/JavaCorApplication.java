709c1de9ee1a7272b71714f18076b46e284b33cb
Aleksey91iv <alekseyiv1991@mail.ru>
2025-12-29 02:22:40 +0300

    Классы приложения и тестового класса приложения

---
+++ src/main/java/ru/coursework2/JavaCOR/JavaCorApplication.java
@@ -0,0 +1,13 @@
+package ru.coursework2.JavaCOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/coursework2/JavaCOR/JavaCorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coursework2.JavaCOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
